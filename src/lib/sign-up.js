2a04e1f22e0791c80a9e99aad55eaa6850d6fb4a
Styrmir <soa61@hi.is>
2023-02-15 23:11:13 +0000

    sign up

--- src/lib/sign-up.js
+++ src/lib/sign-up.js
@@ -1,24 +1,65 @@
+import { registrationValidationMiddleware } from '../lib/validation.js';
 import { createUser } from './users.js';
 
-export async function handleSignup(req, res) {
-  const { username, password } = req.body;
+async function signup(req, res) {
+  let message = '';
 
-  try {
-    const user = await createUser(username, password);
-    if (user) {
-      req.login(user, err => {
-        if (err) {
-          console.error(err);
-          return res.status(500).json({ error: 'Internal server error' });
-        }
+  if (req.method === 'POST') {
+    const { name, password } = req.body;
 
-        return res.json({ success: true });
+    const validation = validationResult(req);
+
+    if (!validation.isEmpty()) {
+      return res.render('/index/signup', {
+        message,
+        title: 'Nýskráning',
+        data: { name, password },
+        errors: validation.errors,
       });
-    } else {
-      return res.status(400).json({ error: 'Could not create user' });
     }
-  } catch (err) {
-    console.error(err);
-    return res.status(500).json({ error: 'Internal server error' });
+
+    const hashedPassword = await hashPassword(password);
+    const user = await createUser({ name, password: hashedPassword });
+
+    if (user) {
+      return res.redirect('/admin/login');
+
+    }
+
+    message = 'Villa kom upp við að nýskrá notanda';
   }
+
+  return res.render('/index/signup', {
+    message,
+    title: 'Nýskráning',
+    data: {},
+    errors: [],
+  });
 }
+
+async function handleSignup(req, res) {
+const { username, password } = req.body;
+
+try {
+  const user = await createUser(username, password);
+  if (user) {
+    req.login(user, err => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Internal server error' });
+      }
+
+      return res.json({ success: true });
+    });
+  } else {
+    return res.status(400).json({ error: 'Could not create user' });
+  }
+} catch (err) {
+  console.error(err);
+  return res.status(500).json({ error: 'Internal server error' });
+}
+}
+export default handleSignup;
+
+indexRouter.get('/signup', catchErrors(signup));
+indexRouter.post('/signup', registrationValidationMiddleware, catchErrors(signup));
