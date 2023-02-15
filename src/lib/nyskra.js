5db0ee7af7bd6eb1c6c2f7440735ee74e764f509
Styrmir <soa61@hi.is>
2023-02-15 23:59:18 +0000

    sign up

---
+++ src/lib/nyskra.js
@@ -0,0 +1,52 @@
+import passport from 'passport';
+import { Strategy } from 'passport-local';
+import { createUser, findById, findByUsername } from './users.js';
+
+passport.use('signup', new Strategy(async (username, password, done) => {
+  try {
+    const user = await findByUsername(username);
+    if (user) {
+      return done(null, false, { message: 'Notandanafn þegar í notkun' });
+    }
+
+    const newUser = await createUser(username, password);
+    return done(null, newUser);
+  } catch (err) {
+    console.error(err);
+    return done(err);
+  }
+}));
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+export function ensureLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+
+  return res.redirect(`/admin/login`);
+}
+
+export const signUp = (req, res, next) => {
+  passport.authenticate('signup', (err, user, info) => {
+    if (err) {
+      console.error(err);
+      return next(err);
+    }
+    if (!user) {
+      return res.status(400).json({ error: info.message });
+    }
+    return res.json(user);
+  })(req, res, next);
+};
--- src/lib/sign-up.js
+++ src/lib/sign-up.js
@@ -1,65 +1,24 @@
-import { registrationValidationMiddleware } from '../lib/validation.js';
 import { createUser } from './users.js';
 
-async function signup(req, res) {
-  let message = '';
+export async function handleSignup(req, res) {
+  const { username, password } = req.body;
 
-  if (req.method === 'POST') {
-    const { name, password } = req.body;
-
-    const validation = validationResult(req);
-
-    if (!validation.isEmpty()) {
-      return res.render('/index/signup', {
-        message,
-        title: 'Nýskráning',
-        data: { name, password },
-        errors: validation.errors,
-      });
-    }
-
-    const hashedPassword = await hashPassword(password);
-    const user = await createUser({ name, password: hashedPassword });
-
+  try {
+    const user = await createUser(username, password);
     if (user) {
-      return res.redirect('/admin/login');
+      req.login(user, err => {
+        if (err) {
+          console.error(err);
+          return res.status(500).json({ error: 'Internal server error' });
+        }
 
+        return res.json({ success: true });
+      });
+    } else {
+      return res.status(400).json({ error: 'Could not create user' });
     }
-
-    message = 'Villa kom upp við að nýskrá notanda';
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: 'Internal server error' });
   }
-
-  return res.render('/index/signup', {
-    message,
-    title: 'Nýskráning',
-    data: {},
-    errors: [],
-  });
 }
-
-async function handleSignup(req, res) {
-const { username, password } = req.body;
-
-try {
-  const user = await createUser(username, password);
-  if (user) {
-    req.login(user, err => {
-      if (err) {
-        console.error(err);
-        return res.status(500).json({ error: 'Internal server error' });
-      }
-
-      return res.json({ success: true });
-    });
-  } else {
-    return res.status(400).json({ error: 'Could not create user' });
-  }
-} catch (err) {
-  console.error(err);
-  return res.status(500).json({ error: 'Internal server error' });
-}
-}
-export default handleSignup;
-
-indexRouter.get('/index/signup', catchErrors(signup));
-indexRouter.post('/index/signup', registrationValidationMiddleware, catchErrors(signup));
